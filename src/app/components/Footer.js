e6b1d7e2ad73e65619418707c1d793b16acaed03
Sydney Du <sydneydu@gmail.com>
2025-07-26 14:42:54 -0400

    More spacing fixes

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
         className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-        href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+        href="https://drive.google.com/file/d/1DXx38Io1ZC-xmbGTDLTjPln1FeXGl0KM/view?usp=sharing"
         target="_blank"
         rel="noopener noreferrer"
         >
--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1 className="font-mono text-center sm:text-left text-2xl sm:text-4xl">
           Sydney Du
@@ -123,14 +123,6 @@ export default function Home() {
           </div>
         </div>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Created with Nextjs
-          </a>
         </div>
       </main>
     </div>
