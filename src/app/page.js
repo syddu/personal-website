3d48d718f00546990820c90fcf695dc067928cb2
Sydney Du <sydneydu@gmail.com>
2025-07-26 11:53:23 -0400

    Added images of organizations

--- src/app/page.js
+++ src/app/page.js
@@ -4,14 +4,35 @@ export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+        <div className="flex gap-20">
+          <Image
+            className="dark:invert"
+            src="/csail.png"
+            alt="CSAIL logo"
+            width={180}
+            height={90}
+            priority
+            style={{ objectFit: "contain" }}
+          />
+          <Image
+            className="dark:invert"
+            src="/noblereach_foundation.png"
+            alt="NobleReach logo"
+            width={440}
+            height={90}
+            priority
+            style={{ objectFit: "contain" }}
+          />
+          <Image
+            className="dark:invert"
+            src="/medialab.png"
+            alt="Media Lab logo"
+            width={165}
+            height={90}
+            priority
+            style={{ objectFit: "contain" }}
+          />
+        </div>
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
