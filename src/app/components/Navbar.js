5f3c67defef1f3b5ddf8ef28488c4167e00a24a3
Sydney Du <sydneydu@gmail.com>
2025-07-26 15:21:07 -0400

    fixed navbar

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -5,31 +5,34 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="container mx-auto flex items-center justify-between">
-        <a className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="/">
+        <Link href="/" passHref>
+          <div className="cursor-pointer rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto">
             <Image
-                src="/home.webp"
-                alt="Home"
-                width={20}
-                height={20}/>
+              src="/home.webp"
+              alt="Home"
+              width={20}
+              height={20}
+            />
             Home
-        </a>
+          </div>
+        </Link>
+
         <ul className="nav-links flex gap-6">
-            <li>
-                <Link href="/about">
-                    <span>About</span>
-                </Link>
-            </li>
-            <li>
-                <Link href="/projects">
-                    <span>Projects</span>
-                </Link>
-            </li>
-            <li>
-                <Link href="/contact">
-                    <span>Contact</span>
-                </Link>
-            </li>
+          <li>
+            <Link href="/about">
+              <span>About</span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/projects">
+              <span>Projects</span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact">
+              <span>Contact</span>
+            </Link>
+          </li>
         </ul>
       </div>
     </nav>
